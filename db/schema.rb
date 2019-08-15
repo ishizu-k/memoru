31cfd26c04a31e02747a579a32d2ece8be381377
ishizu <demekinn_96@yahoo.co.jp>
2019-08-15 13:23:18 +0900

    add: taggingモデル、テーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_103219) do
+ActiveRecord::Schema.define(version: 2019_08_15_042154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 2019_08_14_103219) do
     t.integer "blood"
     t.string "hometown"
     t.string "other"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "meeting_log_id"
+    t.integer "tag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
