6929b2a27293aa0be2a20866ad942fbfb227539f
ishizu <demekinn_96@yahoo.co.jp>
2019-08-18 17:54:49 +0900

    change: ログイン後、マイページに遷移する

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'meeting_logs#index' #暫定的なroot
+  root to: 'users#show' #暫定的なroot
   devise_for :users
   resources :meeting_logs
   resources :tags, only:[:index, :create, :destroy]
