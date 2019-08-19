28ed9ea19c2b2d6edd91b5d8df8395570ba43762
ishizu <demekinn_96@yahoo.co.jp>
2019-08-19 15:26:26 +0900

    add: 対象詳細画面にコメント投稿機能（要ロード）

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,18 @@
 class CommentsController < ApplicationController
   def create
+    @meeting_log = MeetingLog.find(params[:meeting_log_id])
+    @comment = @meeting_log.comments.build(comment_params)
+    respond_to do |format|
+      if @comment.save
+        format.js { render :index }
+      else
+        format.html { redirect_to meeting_log_path(@meeting_log), notice: '投稿できませんでした' }
+      end
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:meeting_log_id, :content)
   end
 end
