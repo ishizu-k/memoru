c7eab59acb599d395736efe036f77a3ce7f39faf
ishizu <demekinn_96@yahoo.co.jp>
2019-08-20 20:00:19 +0900

    add: bootstrap導入

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//
+//= require bootstrap/dist/js/bootstrap.min
 //= require rails-ujs
 //= require activestorage
-//= require jquery_ujs
 //= require turbolinks
 //= require jquery/dist/jquery.js
 //= require_tree .
