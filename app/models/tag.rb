6c238bcd86f941c7f38ca36ca1eb9ef806de30e8
ishizu <demekinn_96@yahoo.co.jp>
2019-08-25 17:28:20 +0900

    update: tagのバリデーション変更（文字数制限30 --> 15）

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 30 }, uniqueness: true
+  validates :name, presence: true, length: { maximum: 15 }, uniqueness: true
   has_many :taggings, dependent: :destroy
   paginates_per 10
 end
