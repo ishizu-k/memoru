dd07c1170b2fe14f0fac14e7d7459b999a7917f7
ishizu <demekinn_96@yahoo.co.jp>
2019-08-26 14:40:06 +0900

    add: シードデータ挿入（暫定）

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+1.times do |n|
+  user = User.new(
+    name: "テストユーザー",
+    email: "test@example.com",
+    password: "111111",
+    password_confirmation: "111111",
+    image: ""
+  )
+  user.save
+end
+
+20.times do |m|
+  name = Faker::Dessert.variety
+  place = Faker::Books::Lovecraft.location
+  memo = Faker::Books::Lovecraft.fhtagn
+  image = Faker::Avatar.image
+  position = Faker::Military.army_rank
+  hometown = Faker::Space.planet
+  meeting_logs = MeetingLog.new(
+    name: name,
+    day: "2019-07-28",
+    place: place,
+    memo: memo,
+    how: "peacefully",
+    image: image,
+    position: position,
+    status: "記憶中",
+    age: "20",
+    look: "",
+    birth: "",
+    blood: "",
+    hometown: hometown,
+    other: "",
+    # user_id: "1"
+  )
+  meeting_logs.save
+end
