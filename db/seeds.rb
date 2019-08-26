b9cbd2affcc14bb8d10d9d1e88fafdd922b70198
ishizu <demekinn_96@yahoo.co.jp>
2019-08-26 18:23:44 +0900

    add: タグのシードデータを挿入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 1.times do |n|
   user = User.create!(
     name: "たなか",
@@ -38,4 +30,9 @@
     )
     end
   user.save!
+end
+
+10.times do |t|
+  tag = Tag.new(name: "タグ#{t}")
+  tag.save!
 end
