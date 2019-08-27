258dcd46baeb72f2d8808a6e1277e93b331e2833
ishizu <demekinn_96@yahoo.co.jp>
2019-08-27 20:21:24 +0900

    add: meeting_logのモデルスペック（dayカラムのバリデーションのテスト）

--- spec/models/meeting_log_spec.rb
+++ spec/models/meeting_log_spec.rb
@@ -2,18 +2,23 @@ require 'rails_helper'
 
 RSpec.describe MeetingLog, type: :model do
   it "nameが空の場合、バリデーションは通らない" do
-    meeting_log = FactoryBot.create(:meeting_log, :skip_validate, name: "")
+    meeting_log = FactoryBot.build(:meeting_log, :skip_validate, name: nil)
     expect(meeting_log).not_to be_valid
   end
 
   it "nameが文字数31以上の場合、バリデーションは通らない" do
-    meeting_log = FactoryBot.create(:meeting_log, :skip_validate, name: "a" * 31)
+    meeting_log = FactoryBot.build(:meeting_log, :skip_validate, name: "a" * 31)
     expect(meeting_log).not_to be_valid
   end
 
   it "nameが重複する場合、バリデーションは通らない" do
     meeting_log = FactoryBot.create(:meeting_log)
-    meeting_log2 = FactoryBot.create(:second_meeting_log, :skip_validate, name: "test-name")
+    meeting_log2 = FactoryBot.build(:second_meeting_log, :skip_validate, name: "test-name")
     expect(meeting_log2).not_to be_valid
+  end
+
+  it "dayが空の場合、バリデーションは通らない" do
+    meeting_log = FactoryBot.build(:meeting_log, :skip_validate, day: nil)
+    expect(meeting_log).not_to be_valid
   end
 end
