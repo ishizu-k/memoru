34d3ae6036365567f2dccbd72aa15ac4577db215
ishizu <demekinn_96@yahoo.co.jp>
2019-08-27 20:51:45 +0900

    add: userのモデルスペック（nameカラムのバリデーションのテスト）

--- spec/factories/user_spec.rb
+++ spec/factories/user_spec.rb
@@ -5,6 +5,9 @@ FactoryBot.define do
     password  {"111111"}
     password_confirmation  {"111111"}
     image  {""}
+    trait :skip_validate do
+      to_create { |user| user.save(validate: false) }
+    end
   end
 
   factory :second_user, class: User do
@@ -13,5 +16,8 @@ FactoryBot.define do
     password  {"111111"}
     password_confirmation  {"111111"}
     image  {""}
+    trait :skip_validate do
+      to_create { |user| user.save(validate: false) }
+    end
   end
 end
