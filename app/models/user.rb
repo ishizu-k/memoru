2715209ae9d98699430bc3ec0aa65b90035b546d
ishizu <demekinn_96@yahoo.co.jp>
2019-10-02 18:11:19 +0900

    fix: アカウント削除できないエラーを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true, length: { maximum: 30 }, uniqueness: true
-  has_many :meeting_logs
-  has_many :tags
+  has_many :meeting_logs, dependent: :destroy
+  has_many :tags, dependent: :destroy
   mount_uploader :image, ImageUploader
 end
